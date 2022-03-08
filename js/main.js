4f84d0cb7f7d49424015b970956a1b2f67d7211d
GASTONSORIA <gastonsoria.18@gmail.com>
2022-03-07 22:11:11 -0300

    1er PRE ENTREGA FINAL

--- js/main.js
+++ js/main.js
@@ -14,3 +14,41 @@ while (cancionInedita != "viento") {
 
 adivinanza () ;
 
+//********************************* */ 1er pre-entrega final ****************************************
+
+//CONSTRUCTOR Y OBJETOS
+
+class Canción{
+    constructor(nombre, duración, género) {
+        this.nombre = nombre;
+        this.duración = duración;
+        this.género = género;
+    }
+    información(){
+        console.log("Nombre:" + this.nombre, "\n Duración: " + this.duración, + "\n género: ", "\n Género: ", this.género);
+    }
+}
+const tantasVeces = new Canción("Tantas veces", "3.21", "Reggae fusión Pop");
+tantasVeces.información();
+
+const soloViento = new Canción("Solo viento", "4.00", "Reggae fusión Jazz"); 
+soloViento.información();
+
+const contradicción = new Canción("Contradicción", "5.00", "Indie Folk");
+contradicción.información();
+
+
+// ARRAYS Y FILTRADOS
+
+
+
+const demos = [
+    { nombre: 'Tantas veces', duración: 3, género: 'Reggae fusión Pop'},
+    { nombre: 'Solo viento', duración: 4, género: 'Reggae fusión Jazz' },
+    { nombre: 'Contradicción', duración: 4, género: 'Indie Folk' }
+]
+
+const playlist1 = demos.reduce((acc, el) => acc + el.duración, 0)
+alert("¿Tenes unos " + playlist1 + " minutos para desenchufarte y escuchar con auriculares? ¡Dale play a la primer nota de voz!") // 12 minutos
+
+
